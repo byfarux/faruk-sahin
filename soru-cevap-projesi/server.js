f29731e2a940f9793a968e95f649bfb2a493830d
byfarux <38378442+byfarux@users.noreply.github.com>
2020-12-18 20:37:18 +0300

    Add files via upload

--- soru-cevap-projesi/server.js
+++ soru-cevap-projesi/server.js
@@ -1,9 +1,11 @@
+'use strict'
 const express = require("express")
 const dotenv = require("dotenv")
 const connectDatabase = require("./helpers/connectDatabase")
 const routers = require("./routers/index")
 const path = require("path")
 const customErrorHandling = require ("./middlewares/customErrorHandling")
+
 
 
 dotenv.config({
@@ -18,6 +20,10 @@ app.use(express.json())
 
 const PORT = process.env.PORT
 
+app.get('/', (req, res) => {
+    res.status(200).send('S.a').end();
+  });
+
 app.use("/api",routers)
 
 app.use(customErrorHandling)
@@ -27,3 +33,5 @@ app.use(express.static(path.join(__dirname,"public")))
 app.listen(PORT,() => {
     console.log(`App Started on : ${PORT} : ${process.env.NODE_ENV}`)
 })
+
+module.exports = app
